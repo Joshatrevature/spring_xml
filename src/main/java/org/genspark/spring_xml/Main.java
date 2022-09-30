5264ae82d16a308e915590fa7b9d13cc00d31112
Joshatrevature <87381776+Joshatrevature@users.noreply.github.com>
2022-09-30 14:01:38 -0400

    Initial commit

---
+++ src/main/java/org/genspark/spring_xml/Main.java
@@ -0,0 +1,5 @@
+package org.genspark.spring_xml;public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/org/genspark/spring_xml/Phone.java
@@ -0,0 +1,2 @@
+package org.genspark.spring_xml;public class Phone {
+}
